6cc57147cbc0d0c7db69a09eef0b9feaff42db5c
Vincent Courtois <babaussine@gmail.com>
2018-07-10 05:25:27 +0200

    Try another configuration

--- config.ru
+++ config.ru
@@ -1,8 +1,9 @@
 require 'bundler'
 Bundler.require(ENV['RACK_ENV'].to_sym || :development)
 
-service = Arkaan::Utils::MicroService.instance
-  .from_location(__FILE__)
-  .in_websocket_mode
+service = Arkaan::Utils.MicroService.instance
+  .register_as('messages')
+  .in_standard_mode
 
 map('/websockets') { run Controllers::Websockets.new }
+map(service.path) { run Controllers::Messages.new }
--- controllers/websockets.rb
+++ controllers/websockets.rb
@@ -1,8 +1,7 @@
 module Controllers
   # Controller handling the websockets, creating it and receiving the commands for it.
   # @author Vincent Courtois <courtois.vincent@outlook.com>
-  class Websockets < Arkaan::Utils::Controller
-
+  class Websockets < Sinatra::Base
     get '/' do
       session = check_session 'messages'
       if !request.websocket?
@@ -10,13 +9,6 @@ module Controllers
       else
         Services::Websockets.create(session.id.to_s, request.websocket)
       end
-    end
-
-    post '/messages' do
-      session = check_session 'messages'
-      check_presence 'message', 'receiver'
-      Services::Websockets.send_to_user(receiver, params['message'], params['data'] || {})
-      render 200, {message: 'transmitted'}.to_json
     end
   end
 end
