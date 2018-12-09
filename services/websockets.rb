8121b2321c6de8c6b63f6af67db1a61ccdff30d0
Babausse <courtois.vincent@outlook.com>
2018-12-09 18:48:53 +0100

    Add traces, not to be removed in the future

--- services/websockets.rb
+++ services/websockets.rb
@@ -19,13 +19,17 @@ module Services
     # @param session_id [String] the unique identifier of the session to associate the socket to.
     # @param websocket [Object] the websocket object associated to the session.
     def create(session_id, websocket)
+      session = Arkaan::Authentication::Session.where(_id: session_id).first
       websocket.onopen {
-        sockets[session_id] = websocket
-        session = Arkaan::Authentication::Session.where(_id: session_id).first
+        @sockets[session_id] = websocket
         instance_id = Arkaan::Utils::MicroService.instance.instance.id.to_s
         session.update_attribute(:websocket_id, instance_id) if !session.nil?
+        logger.info("Création du canal de communication pour une session de #{session.account.username}")
       }
-      websocket.onclose { sockets.delete(session_id) }
+      websocket.onclose {
+        @sockets.delete(session_id)
+        logger.info("Fermeture du canal de communication pour une session de #{session.account.username}")
+      }
     end
 
     # Sends a message to the goven user in its dedicated websocket.
@@ -33,8 +37,14 @@ module Services
     # @param message [String] the type of message you want to send.
     # @param data [Hash] a JSON-compatible hash to send as a JSON string with the message type.
     def send_to_sessions(session_ids, message, data)
+      logger.info("Transmission d'un message #{message} avec les données suivates :")
+      logger.info(data.to_json)
+      logger.info('aux utilisateurs :')
       session_ids.each do |session_id|
+        session = Arkaan::Authentication::Session.where(_id: session_id).first
+        logger.info("-> #{session.account.username}")
         if !sockets[session_id].nil?
+          logger.info("     Dont le websocket a été trouvé correctement !")
           EM.next_tick do
             sockets[session_id].send({message: message, data: data}.to_json)
           end
