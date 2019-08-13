19b9d7dbf6d98cab651055f0bd95bafe54d007bd
Babausse <courtois.vincent@outlook.com>
2019-08-13 14:27:45 -0700

    Add the monitoring to the websockets service

--- controllers/websockets.rb
+++ controllers/websockets.rb
@@ -3,7 +3,7 @@ require 'sinatra/custom_logger'
 module Controllers
   # Controller handling the websockets, creating it and receiving the commands for it.
   # @author Vincent Courtois <courtois.vincent@outlook.com>
-  class Websockets < Arkaan::Utils::ControllerWithoutFilter
+  class Websockets < Arkaan::Utils::Controllers::Base
 
     load_errors_from __FILE__
 
